5d83b15ea548bbe58a0111b19cb1bbd144dffed3
rflores <104962825+rfloresj@users.noreply.github.com>
2026-01-14 13:48:25 -0600

    feat: navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import { useGSAP } from '@gsap/react';
+import { navLinks } from '../../constants';
+import gsap from 'gsap';
+
+const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: 'nav',
+        start: 'bottom top',
+      },
+    });
+  });
+
+  return (
+    <nav>
+      <div>
+        <a href='#home' className='flex items-center gap-2'>
+          <img src='/images/logo.png' alt='logo' />
+          <p>Velvet Pour</p>
+        </a>
+
+        <ul>
+          {navLinks.map((link) => (
+            <li key={link.id}>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
